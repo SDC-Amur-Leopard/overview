aa5b9cc7a71cf838fa9b771a7d4594b97950d69c
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-01-05 11:16:03 -0700

    added host to env file

--- src/database/database.ts
+++ src/database/database.ts
@@ -5,9 +5,10 @@ dotenv.config()
 
 const user = process.env.PSQL_USER as string
 const password = process.env.PSQL_PASS as string
+const host = process.env.PSQL_HOST as string
 
 export const sequelize: Sequelize = new Sequelize('sdc', user, password, {
-  host: 'localhost',
+  host: host,
   dialect: 'postgres',
   logging: false
 })
